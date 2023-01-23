6c3c7b1a17a55b8bc1b69141a8d19b505dac6ca9
elkirko <kirkhagglund@gmail.com>
2023-01-23 17:30:12 -0600

    added questions and answers

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,3 @@
-/*Tasks to be done:*/
-
 //Assignment section for required variables
 let scoreDisplay = document.querySelector(".score-display");
 let timeDisplay = document.getElementById("time-display");
@@ -9,14 +7,25 @@ let questionSlide = document.querySelector(".question");
 let mainTitle = document.querySelector("h1");
 let questionContent = document.querySelector(".question-content");
 let secondsLeft = 75;
-let answerOptions = document.querySelector(".answer-options");
+let answerOne = document.getElementById("answer-one");
+let answerTwo = document.getElementById("answer-two");
+let answerThree = document.getElementById("answer-three");
+let answerFour = document.getElementById("answer-four");
 questionContent.hidden = true;
+let answerOptions = document.querySelector(".answer-options");
 answerOptions.hidden = true;
-let answerOne = document.querySelector("#answer-one");
-let answerTwo = document.querySelector("#answer-two");
-let answerThree = document.querySelector("#answer-three");
-let answerFour = document.querySelector("#answer-four");
-let functionArray = [answerOne, answerTwo, answerThree, answerFour];
+
+//Question and answer array assignments
+let questionList = ["Roses are:", "Violets are:", "I'm learning:", "What about:"];
+let answerOneArr = ["Red", "Also Red", "To Speak Klingon", "Yew"];
+let answerTwoArr = ["Stinky", "Weapons", "The Art of War", "You"];
+let answerThreeArr = ["Delicious", "Blue", "JavaScript", "New"];
+let answerFourArr = ["Insulting", "Not Real", "The Meaninglessness of Life", "Shoe"];
+let currentQuestion = 0;
+let correctAnswerARR = ["Red", "Blue", "JavaScript", "You"];
+let finalScore = 0;
+
+//Function section
 
 //Create function to start game with an event listener on start game button
 let startGame = function() {
@@ -24,7 +33,7 @@ let startGame = function() {
     generateQuiz();
 };
 
-startButton.addEventListener("click", startGame);
+
 
 
 //Create a start timer function to nest inside the start game function
@@ -37,6 +46,8 @@ function startTimer() {
             clearInterval(gameTime);
             questionContent.textContent = "You ran out of time!";
             answerOptions.hidden = true;
+            startButton.hidden = false;
+            secondsLeft = 75;
         };
     }, 1000);
     startButton.hidden = true;
@@ -47,38 +58,61 @@ function startTimer() {
 
 //Create a generate quiz function to nest inside the start game function
 function generateQuiz() {
-    questionContent.textContent = "Arrays in JavaScript can be used to store:"
-
-    answerOne.addEventListener("click", wrongAnswer());
-    answerTwo.addEventListener("click", wrongAnswer());
-    answerThree.addEventListener("click", wrongAnswer());
-    answerFour.addEventListener("click", rightAnswer());    
+    questionContent.textContent = questionList[currentQuestion];
+    answerOne.textContent = answerOneArr[currentQuestion];
+    answerTwo.textContent = answerTwoArr[currentQuestion];
+    answerThree.textContent = answerThreeArr[currentQuestion];
+    answerFour.textContent = answerFourArr[currentQuestion]; 
 };
+
+
 
 //Create two functions, one for right answers and one for wrong answers
-function rightAnswer() {
-
+function answerEval(event) {
+    console.log(event.target.textContent);
+    if (event.target.textContent !== correctAnswerARR[currentQuestion]) {      
+        secondsLeft = secondsLeft - 15;     
+    };
+    /*if (questionList[3]) {
+        endQuiz();
+    };*/
+    /*if(currentQuestion >= currentQuestion.length) {
+        endQuiz();
+    }*/
+    currentQuestion++;
+    generateQuiz();
 };
 
-function wrongAnswer() {
-
+function endQuiz() {
+    questionContent.hidden = true;
+    answerOptions.hidden = true;
+    finalScore = secondsLeft;
+    console.log(finalScore);
+    clearInterval(gameTime);
 };
 
-function questionOne() {
+//Event listener section
+startButton.addEventListener("click", startGame);
+answerOne.addEventListener('click', answerEval);
+answerTwo.addEventListener('click', answerEval);
+answerThree.addEventListener('click', answerEval);
+answerFour.addEventListener('click', answerEval);
 
+/*function questionOne() {
+    questionList.textContent = "Roses are:";
 };
 
 function questionTwo() {
-
+    questionList.textContent = "Violets are:";
 };
 
 function questionThree() {
-
+    questionList.textContent = "I'm learning:"
 };
 
 function questionFour() {
-
-};
+    questionListt.textContent = "What about:";
+};*/
 /*Quiz needs to generate a new window with a set of four buttons with one correct answer
 above the buttons will be text that reads the question
 if the correct answer is hit, move on to next question
